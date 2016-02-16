80808fee664edc58539f1f8d456738df16279554
greatsong <greatsong21@gmail.com>
2016-02-17 01:30:57 +0900

    Initial Commit

---
+++ githubTest01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  githubTest01
+//
+//  Created by greatsong on 2/17/16.
+//  Copyright © 2016 greatsong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
